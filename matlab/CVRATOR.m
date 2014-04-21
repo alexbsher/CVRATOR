d600b0035cc398a10f1a4bddcd62d967685fce9c
alexbsher <alexbsher@gmail.com>
2014-04-20 23:02:54 -0400

    Now pulls data out and stores in matrix. Next up SVM.

--- matlab/CVRATOR.m
+++ matlab/CVRATOR.m
@@ -4,6 +4,12 @@ feat_root = '../features/';
 root_ls = dir(img_root);
 labels = {root_ls.name};
 
+names = cell(100000, 1);
+data = zeros(10000, 2659);
+out_labels = cell(100000, 1);
+
+count = 1;
+
 % Start at 3, as first two entries are '.' and '..'
 for i=3:length(labels)-1
     
@@ -28,10 +34,23 @@ for i=3:length(labels)-1
             feature_loc = strcat(feat_root, labels{i}, '/', artists{j}, '/', paint_name, '_classemes', '.dat');
             vec = load_float_matrix(feature_loc{1});
             
+            names(count) = strcat(artists{j}, '/', paint_name);
+            data(count, :) = vec';
+            out_labels(count) = {labels{i}};
+            
+            count = count + 1;
+            
         end
         
     end
     
-   
-    
 end
+
+[last_name, ~] = ind2sub(size(data), find(data, 1, 'last'));
+
+names = names(1:last_name);
+data = data(1:last_name, :);
+out_labels = out_labels(1:last_name);
+
+
+
