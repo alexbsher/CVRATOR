614f744ca1cb6c029d7c0c184667fd9abf71fc06
Alex <alexbsher@gmail.com>
2014-05-03 21:11:22 -0700

    Added .dat files for new images and started function to trian learner.

---
+++ matlab/trainLearner.m
@@ -0,0 +1,28 @@
+function [ svm_struct ] = trainLearner( num_train, num_test, labels )
+
+    addpath('./libsvm-3.18/matlab/')
+
+    [testDat, trainDat] = getData(num_train, num_test, labels);
+    
+    testData = [testDat.dat]';
+    trainData = [trainDat.dat]';
+    testLabels = {testDat.label}';
+    trainLabels = {trainDat.label}';
+    
+    labels = [1:num_test:size(testData, 1)];
+    dis_labels = testLabels(labels);
+    
+    train_labels = [1:num_train:size(trainData, 1)];
+    train_dis_labels = trainLabels(train_labels);
+    
+    test_labs = cellfun(@(x) find(ismember(dis_labels, x)), testLabels);
+    train_labs = cellfun(@(x) find(ismember(dis_labels, x)), trainLabels);
+    
+    svm_struct = svmtrain(train_labs, trainData, '-t 2');
+    
+    outputs = svmpredict(test_labs, testData, svm_struct);
+    
+    confusion_mat = confusionmat(test_labs, outputs)
+    dis_labels
+end
+
